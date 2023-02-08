3656578f853aec1d3292a1b68242fb81c2127b03
Bl4ckFlow <raouflarbi352@gmail.com>
2023-02-08 17:14:20 +0100

    fproject

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>So what is UNFLEX?</span> <br> UNFLEX is an IT company that was created in 2015 by a group of students that graduated from a the Higher School of Artificial Intelligence,  "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
